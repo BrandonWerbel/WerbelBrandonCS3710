c59d632a1d8f21f7c5faa48ae3a73a82d5a639c8
BrandonWerbel <56608573+BrandonWerbel@users.noreply.github.com>
2025-02-25 21:46:39 -0500

    Add comments to portscan

--- PythonPortScanner/portscan.py
+++ PythonPortScanner/portscan.py
@@ -11,12 +11,14 @@ print("Please enter the range of ports you would like to scan on the target")
 start_port = input("Enter a start port (inclusive): ")
 end_port = input("enter an end port (inclusive): ")
 
+# generates addresses for each port in range, +1 is to make end port inclusive
 addresses = [(target, i) for i in range(int(start_port), int(end_port) + 1)]
 
 print("Scanning started at " + str(datetime.now())) # https://www.w3schools.com/python/python_datetime.asp
 
 for address in addresses:
     try:
+        # https://docs.python.org/3/library/socket.html#socket.create_connection
         sock = create_connection(address, TIMEOUT) # will throw error if port is closed
     except:
         print(f"Port {address[1]} is closed")
